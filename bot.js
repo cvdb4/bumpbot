d400354ec8f0690a04d85c110021771bb7f40cd6
cvdb4 <36575698+cvdb4@users.noreply.github.com>
2018-02-17 22:38:50 +0100

    first try

---
+++ bot.js
@@ -0,0 +1,20 @@
+var Discord = require('discord.js');
+var bot = new Discord.Client({
+    token: "NDE0MjQ0MTU3NDY1NTU5MDgw.DWonRQ.YygnnhP5FBFVQSY_hzjBaR20UtM",
+     autorun: true
+});
+
+bot.on('ready', function() {
+    console.log(bot.username + " - (" + bot.id + ")");
+});
+
+bot.on('message', function(user, userID, channelID, message, rawEvent) {
+    if (message === "!test") { // command to trigger
+      var interval = setInterval (function (){
+        bot.sendMessage({
+          to: 413422410415603713,
+          message: "=bump" // message to send
+        });
+      }, 5000); // time between each interval in milliseconds
+    }
+});
