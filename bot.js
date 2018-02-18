80a8d8088e802842047c7592d0e5352569fd53d1
cvdb4 <36575698+cvdb4@users.noreply.github.com>
2018-02-19 00:03:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ bot.on('message', function(user, userID, channelID, message, rawEvent) {
     if (message === "!test") { // command to trigger
       var interval = setInterval (function (){
         bot.sendMessage({
-          to: 413422410415603713,
+          to: #office,
           message: "=bump" // message to send
         });
       }, 5000); // time between each interval in milliseconds
