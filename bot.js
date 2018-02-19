629d66290a3b1e7d2a2af9ca9fe586ff5e337517
cvdb4 <36575698+cvdb4@users.noreply.github.com>
2018-02-19 03:33:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,9 @@ bot.on('ready', function() {
     console.log(bot.user.username);
 });
 
-bot.on('message', (m) => {
-  if (m.content === 'ping') m.reply('pong');
+bot.on('message', function m {
+    if (message.content === "$loop") {
+        var interval = setInterval(function() {
+            m.channel.send('test');}, 1000);
+    }
 });
-
