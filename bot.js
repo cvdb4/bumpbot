b112dfbfd8f1acb06e8485df6299fd30e6ad5d1b
cvdb4 <36575698+cvdb4@users.noreply.github.com>
2018-02-19 17:42:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 var Discord = require('discord.js');
 var bot = new Discord.Client()
 
-bot.login("NDE0MjQ0MTU3NDY1NTU5MDgw.DWyNxw.UjvIFd6KlaFy4jT63Y9dAgQYumg");
+bot.login("");
 
 bot.on('ready', function() {
     console.log(bot.user.username);
