1841a699b622eb9e13cf213667044cd0baede8b9
cvdb4 <36575698+cvdb4@users.noreply.github.com>
2018-02-19 03:37:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,13 @@ var bot = new Discord.Client()
 
 bot.login("NDE0MjQ0MTU3NDY1NTU5MDgw.DWthZw.gadOqltLRxDrTqEr8Y6J3u8m2eY");
 
+bot.on('ready', function() {
+    console.log(bot.user.username);
+});
 
-bot.on('message', function m {
+bot.on('message', function() {
     if (message.content === "$loop") {
         var interval = setInterval(function() {
-            m.channel.send('test');}, 1000);
+            message.channel.send('test');}, 1000);
     }
 });
