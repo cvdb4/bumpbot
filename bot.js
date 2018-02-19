11db32f1c0a848518046491d88f299e27f3e8339
cvdb4 <36575698+cvdb4@users.noreply.github.com>
2018-02-19 03:35:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,9 +3,6 @@ var bot = new Discord.Client()
 
 bot.login("NDE0MjQ0MTU3NDY1NTU5MDgw.DWthZw.gadOqltLRxDrTqEr8Y6J3u8m2eY");
 
-bot.on('ready', function() {
-    console.log(bot.user.username);
-});
 
 bot.on('message', function m {
     if (message.content === "$loop") {
