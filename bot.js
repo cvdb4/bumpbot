a5f190a807f11345233e1995197d0f19516e909b
cvdb4 <36575698+cvdb4@users.noreply.github.com>
2018-02-19 01:51:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,16 @@
-var DiscordClient = require('discord.io');
-var bot = new Discord.Client({
-    autorun: true,
-    email: "", // email
-    password: "", // password
-    //OR
-    token: ""
-});
+var Discord = require('discord.js');
+var bot = new Discord.Client()
+
 bot.login("NDE0MjQ0MTU3NDY1NTU5MDgw.DWthZw.gadOqltLRxDrTqEr8Y6J3u8m2eY");
 
 bot.on('ready', function() {
-    console.log(bot.username + " - (" + bot.id + ")");
+    console.log(bot.user.username);
 });
 
-bot.on('message', function(user, userID, channelID, message, rawEvent) {
-    if (message === "!test") { // command to trigger
-      var interval = setInterval (function (){
-        bot.sendMessage({
-          to: 413422410415603713,
-          message: "=bump" // message to send
-        });
-      }, 1000); // time between each interval in milliseconds
+bot.on('message', function() {
+    if (message.content === "$loop") { 
+      var interval = setInterval (function () {
+        message.channel.send("123")
+      }, 1 * 1000); 
     }
 });
